66e04f68290c577d6c7296840ba1130f184f7ab7
David <Murnto@users.noreply.github.com>
2018-11-13 14:03:33 +1100

    Have AssemblingMachine extend EntityWithHealth

--- src/types/factorio.assemblignmachine.d.ts
+++ src/types/factorio.assemblignmachine.d.ts
@@ -1,40 +1,26 @@
-export interface AssemblingMachine {
+import {EntityWithHealth} from "./factorio.entity";
+
+export interface AssemblingMachine extends EntityWithHealth {
     alert_icon_shift?: number[];
     allowed_effects?: AllowedEffect[];
     always_draw_idle_animation?: boolean;
     close_sound?: CloseSoundClass;
-    corpse: Corpse;
     crafting_categories: string[];
     crafting_speed: number;
     drawing_box?: Array<number[]>;
-    dying_explosion?: DyingExplosion;
     energy_source: EnergySource;
     energy_usage: string;
-    fast_replaceable_group?: string;
     flags: Flag[];
     flammability: number;
     fluid_boxes?: FluidBox[] | FluidBoxesClass;
     has_backer_name?: boolean;
-    healing_per_tick: number;
-    icon?: string;
-    icon_size: number;
-    icons?: Icon[];
     idle_animation?: IdleAnimation;
     ingredient_count: number;
-    max_health: number;
-    minable: Minable;
-    mined_sound?: MinedSoundClass;
     module_specification?: ModuleSpecification;
-    name: string;
     open_sound?: CloseSoundClass;
-    order?: string;
     pipe_covers?: PipeCovers;
-    repair_sound?: MinedSoundClass;
-    repair_speed_modifier: number;
     scale_entity_info_icon?: boolean;
     source_inventory_size?: number;
-    title: string;
-    type: AssemblingMachineType;
     working_visualisations_disabled?: WorkingVisualisationsDisabled[];
 }
 
@@ -53,17 +39,6 @@ export interface CloseSoundClass {
 export enum Filename {
     BaseSoundMachineCloseOgg = "__base__/sound/machine-close.ogg",
     BaseSoundMachineOpenOgg = "__base__/sound/machine-open.ogg",
-}
-
-export enum Corpse {
-    BigRemnants = "big-remnants",
-    MediumRemnants = "medium-remnants",
-    SmallRemnants = "small-remnants",
-}
-
-export enum DyingExplosion {
-    BigExplosion = "big-explosion",
-    MediumExplosion = "medium-explosion",
 }
 
 export interface EnergySource {
@@ -188,20 +163,6 @@ export interface The3_PipeConnection {
     position: number[];
 }
 
-export interface Icon {
-    icon: string;
-    shift?: number[];
-    tint?: Tint;
-    scale?: number;
-}
-
-export interface Tint {
-    g: number;
-    r: number;
-    a: number;
-    b: number;
-}
-
 export interface IdleAnimation {
     layers: IdleAnimationLayer[];
 }
@@ -229,16 +190,8 @@ export interface Minable {
     hardness?: number;
 }
 
-export interface MinedSoundClass {
-    filename: string;
-}
-
 export interface ModuleSpecification {
     module_slots: number;
-}
-
-export enum AssemblingMachineType {
-    AssemblingMachine = "assembling-machine",
 }
 
 export interface WorkingVisualisationsDisabled {
