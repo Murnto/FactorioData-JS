3e7e0d949018f8dd64d58ee24f911a95800564ed
David <Murnto@users.noreply.github.com>
2019-01-08 15:15:22 +1100

    ItemInfo: Remove legacy addToGraph

--- src/Components/packItemInfo.tsx
+++ src/Components/packItemInfo.tsx
@@ -3,16 +3,11 @@ import { Item } from "../types/factorio.item";
 import { PrototypeIcon } from "./Minor/prototypeIcon";
 import { RecipeList } from "./Minor/recipeList";
 import { PackLoadedData } from "../packLoadedData";
-import FormGroup from "reactstrap/lib/FormGroup";
-import Input from "reactstrap/lib/Input";
-import Form from "reactstrap/lib/Form";
-import Button from "reactstrap/lib/Button";
 import { Recipe } from "../types/factorio.recipe";
 import { PrototypeHasIcon } from "../types/factorio.prototype";
 import { EntityInfoBox } from "./InfoBox/entityInfoBox";
 
 interface PackItemInfoState {
-  amount: string;
   entity: PrototypeHasIcon | null;
   item: Item | null;
   recipesProducing: Recipe[] | null;
@@ -27,7 +22,6 @@ export class PackItemInfo extends React.Component<
     super(p, s);
 
     this.state = {
-      amount: "0",
       entity: null,
       item: null,
       recipesProducing: null,
@@ -52,13 +46,7 @@ export class PackItemInfo extends React.Component<
   public render() {
     const { match, data } = this.props;
     const { itemType, itemName, packId } = match.params;
-    const {
-      amount,
-      entity,
-      item,
-      recipesProducing,
-      recipesUsedIn
-    } = this.state;
+    const { entity, item, recipesProducing, recipesUsedIn } = this.state;
 
     console.log("[PackItemInfo] Render with", packId, itemType, itemName, item);
 
@@ -75,19 +63,6 @@ export class PackItemInfo extends React.Component<
             <h2>
               <PrototypeIcon item={item} /> {item.title}
             </h2>
-            <Form inline>
-              <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                <Input
-                  type="number"
-                  name="number"
-                  value={amount}
-                  onChange={this.onInput}
-                  placeholder="1"
-                />
-              </FormGroup>
-
-              <Button onClick={this.addItemToGraph}>Add</Button>
-            </Form>
           </div>
         )) || (
           <h2>
@@ -120,30 +95,11 @@ export class PackItemInfo extends React.Component<
       this.props.match.params.itemType !== nextProps.match.params.itemType ||
       this.props.match.params.itemName !== nextProps.match.params.itemName ||
       this.props.data.packId !== nextProps.data.packId ||
-      this.state.amount !== nextState.amount ||
       this.state.item !== nextState.item ||
       this.state.recipesUsedIn !== nextState.recipesUsedIn ||
       this.state.recipesProducing !== nextState.recipesProducing
     );
   }
-
-  private addItemToGraph = () => {
-    const { match, data } = this.props;
-    const { itemType, itemName } = match.params;
-    const itemObj = data.findItem(itemType, itemName);
-
-    if (itemObj === null) {
-      return;
-    }
-
-    fetch("http://127.0.0.1:8500/add_item", {
-      body: `${itemObj.type}/${itemObj.name}/${this.state.amount}`,
-      method: "POST",
-      mode: "no-cors"
-    }).then(r => {
-      console.log(r.ok, r.status, r);
-    });
-  };
 
   private initInfo() {
     const { match, data } = this.props;
@@ -163,10 +119,4 @@ export class PackItemInfo extends React.Component<
 
     console.timeEnd(`initInfo:${itemType}:${itemName}`);
   }
-
-  private onInput = (e: React.FormEvent<HTMLInputElement>) => {
-    this.setState({
-      amount: e.currentTarget.value
-    });
-  };
 }
